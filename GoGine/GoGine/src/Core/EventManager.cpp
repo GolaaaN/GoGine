8579aac2bf12141df0e83926f99d2f0ae4d6a5c0
user2428 <ilaigolan100@gmail.com>
2024-09-11 10:18:39 +0300

    Created Event Manager API

--- GoGine/GoGine/src/Core/EventManager.cpp
+++ GoGine/GoGine/src/Core/EventManager.cpp
@@ -1,1 +1,47 @@
 #include "EventManager.h"
+
+void GOG::EventManager::Subscribe(const EventType& e, const std::unique_ptr<IEventHandlerWrapper> handler)
+{
+	auto subscribers = m_subscribers.find(e);
+    if (subscribers != m_subscribers.end()) {
+        auto& handlers = subscribers->second;
+        for (auto& it : handlers) {
+            if (it->GetEventType() == handler->GetEventType()) {
+                return;
+            }
+        }
+        handlers.emplace_back(std::move(handler));
+    }
+    else {
+        m_subscribers[e].emplace_back(std::move(handler));
+    }
+}
+
+void GOG::EventManager::UnSubscribe(const EventType& e)
+{
+}
+
+void GOG::EventManager::TriggerEvent(const Event& e)
+{
+    for (auto& handler : m_subscribers[e.GetEventType()]) {
+        handler->Exec(e);
+    }
+}
+
+void GOG::EventManager::QueueEvent(std::unique_ptr<Event> e)
+{
+    m_events_queue.emplace_back(std::move(e));
+}
+
+void GOG::EventManager::DispatchEvents()
+{
+    for (auto eventIt = m_events_queue.begin(); eventIt != m_events_queue.end();) {
+        if (!eventIt->get()->Handled) {
+            TriggerEvent(*eventIt->get());
+            eventIt = m_events_queue.erase(eventIt);
+        }
+        else {
+            ++eventIt;
+        }
+    }
+}
--- GoGine/GoGine/src/Core/EventManager.h
+++ GoGine/GoGine/src/Core/EventManager.h
@@ -9,7 +9,11 @@ namespace GOG {
 	class EventManager
 	{
 	public:
-
+		void Subscribe(const EventType &e, const std::unique_ptr<IEventHandlerWrapper> handler);
+		void UnSubscribe(const EventType& e);
+		void TriggerEvent(const Event& e);
+		void QueueEvent(std::unique_ptr<Event> e);
+		void DispatchEvents();
 
 	private:
 		std::vector<std::unique_ptr<Event>> m_events_queue;
